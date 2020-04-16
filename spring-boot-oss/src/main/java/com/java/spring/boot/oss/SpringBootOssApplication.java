fb4f3ade9a419163843e62b9986a9d94542b24f1
liuyunkai <632233796@qq.com>
2020-04-16 14:57:36 +0800

    http ---> https

--- spring-boot-oss/src/main/java/com/java/spring/boot/oss/SpringBootOssApplication.java
+++ spring-boot-oss/src/main/java/com/java/spring/boot/oss/SpringBootOssApplication.java
@@ -1,16 +1,59 @@
 package com.java.spring.boot.oss;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Administrator
  */
 @SpringBootApplication
-public class SpringBootOssApplication {
+public class SpringBootOssApplication implements WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootOssApplication.class, args);
+    }
+
+    //拦截所有请求
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint constraint = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(httpConnector());
+        return tomcat;
+    }
+    //配置http转https
+    @Bean
+    public Connector httpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(80);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(443);
+        return connector;
+    }
+    //这里设置默认端口为443，即https的，如果这里不设置，会https和http争夺80端口
+    @Override
+    public void customize(ConfigurableServletWebServerFactory container) {
+        container.setPort(443);
     }
 
 }
